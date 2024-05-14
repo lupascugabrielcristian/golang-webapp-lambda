9433671a4aa8b9239fa962e090268cf1dae59f72
CristianGabriel <desktop@binaryfusion.ro>
2024-05-14 10:13:36 +0300

    Initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module example.com/on_path_robotics2
+
+go 1.19
+
+require github.com/aws/aws-lambda-go v1.47.0 // indirect
---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func main() {
+	lambda.Start(handler)
+}
+
+func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	var person Person
+	fmt.Println("urmeaza person")
+	err := json.Unmarshal([]byte(request.Body), &person)
+	if err != nil {
+		return events.APIGatewayProxyResponse{}, err
+	}
+
+	fmt.Println("am luat person")
+
+	msg := fmt.Sprintf("Hello from %v %v by lambda fc", *person.FirstName, *person.LastName)
+	responseBody := ResponseBody{
+		Message: &msg,
+	}
+
+	jbytes, _ := json.Marshal(responseBody)
+	jstr := string(jbytes)
+
+	response := events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Body:       jstr,
+	}
+	return response, nil
+}
+
+type ResponseBody struct {
+	Message *string `json:"message"`
+}
+
+type Person struct {
+	FirstName *string `json:"firstName"`
+	LastName  *string `json:"lastName"`
+}
